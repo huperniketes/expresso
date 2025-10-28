f2919a42d8116d6a2a4689779282ca45a77195b1
Alfonso Guerra <huperniketes@gmail.com>
2025-10-28 15:53:19 -0400

    Legalese attached to the project: license and copyright in file and source code preambles.

--- src/cli/repl.c
+++ src/cli/repl.c
@@ -1,3 +1,51 @@
+/*
+ * Expresso
+ * repl.c
+ *
+ * The functions which define the processes of the REPL's main loop:
+ * read, eval, & print, and its initialization and cleanup.
+ *
+ * Expresso is an interactive command-line tool for evaluating expressions.
+ * Expresso supports basic data types (integers, characters, strings),
+ * literals, operators (arithmetic, logical, comparison, bitwise, string
+ * manipulation), and nested expressions using C syntax (e.g., precedence,
+ * parentheses). Key constraint: No mutable state—every input is a pure
+ * expression that evaluates to an immutable value.
+ *
+ * ========================================================================
+ *
+ * © Copyright 2025 Alfonso Guerra
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *  *  Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *  *  Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *  *  Neither the name of the copyright holder nor the names of
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 #include "repl.h"
 #include "parser_wrapper.h" // For C++ parser interface
 #include "evaluator.h"      // For evaluator
